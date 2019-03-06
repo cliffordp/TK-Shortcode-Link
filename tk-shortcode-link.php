2c8688f4a0299cb2065426a0588bf8a09d10f47a
Clifford Paulick <tko@tourkick.com>
2019-03-05 21:32:03 -0600

    Cleanup code

--- tk-shortcode-link.php
+++ tk-shortcode-link.php
@@ -4,11 +4,11 @@ Plugin Name: TK Shortcode Link
 Description: Create links with a shortcode
 Author: TourKick
 Version: 1.2.1
-Author URI: http://tourkick.com/?utm_source=wordpressdotorg&utm_medium=tkshortcodelinkplugin&utm_content=authoruri&utm_campaign=tkshortcodelinkplugin
+Author URI: https://tourkick.com/?utm_source=wordpressdotorg&utm_medium=tkshortcodelinkplugin&utm_content=authoruri&utm_campaign=tkshortcodelinkplugin
 License: GPL version 3 or any later version
 */
 
-function tklink_shortcode( $atts , $content = null ) {
+function tklink_shortcode( $atts, $content = null ) {
 
   // Attributes
 	extract( shortcode_atts(
@@ -19,11 +19,6 @@ function tklink_shortcode( $atts , $content = null ) {
 		), $atts )
 	);
 
-	// Escaping
-/*
-	$esccontent = esc_html($content); // not used because maybe you want HTML as the content (e.g. image)
-	$escurl = esc_html($url); // not used because maybe you want a javascript URL
-*/
 	$esctarget = esc_html($target);
 	$escclass = esc_html($class);
 
@@ -40,6 +35,5 @@ function tklink_shortcode( $atts , $content = null ) {
 		return '<a class="' . $escclass . '" href="' . $url . '" target="_' . $esctarget . '">' . $content . '</a>';
 	}
 }
-// add_shortcode( 'link', 'tklink_shortcode' );
-// conflicted with PageLines Theme ( https://github.com/pagelines/DMS/blob/Dev/includes/class.shortcodes.php#L119 )
-add_shortcode( 'tklink', 'tklink_shortcode' ); // does not conflict with PageLines
+
+add_shortcode( 'tklink', 'tklink_shortcode' );
